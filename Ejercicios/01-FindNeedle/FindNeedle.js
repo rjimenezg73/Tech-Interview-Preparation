e2248a79d03e943a0cf9903d59c3a74d1d0a5c48
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-09-12 23:32:12 -0500

    dia 1, hw 1

--- Ejercicios/01-FindNeedle/FindNeedle.js
+++ Ejercicios/01-FindNeedle/FindNeedle.js
@@ -1,6 +1,36 @@
 function FindNeedle (haystack, needle) {
   // Your code here:
 
+  console.log(haystack);
+  console.log(needle);    
+
+//   for(let i = 0; i < haystack.legth - needle.legth + 1; i++){
+//     const sliced = haystack.slice(i, i + needle.legth);
+//     console.log(sliced);
+//     if(needle === sliced)
+//       return i;
+//   }
+//   return -1
+// }
+
+
+  for (let i = 0; i < haystack.length - needle.length + 1; i++) {
+    for (let j = 0; j < needle.length; j++) {     
+      if (haystack[i + j] !== needle[j]) break;
+      if (j === needle.length - 1) return i;
+    }
+  }
+  return -1;
 }
+
+
+
+console.log(FindNeedle('react-redux', 'redux'));
+console.log(FindNeedle('aaapink', 'pink'));
+console.log(FindNeedle('rereredux', 'reredux'));
+console.log(FindNeedle('margarita', 'reredux'));
+
+
+
 
 module.exports = FindNeedle
--- Ejercicios/02-SumArray/SumArray.js
+++ Ejercicios/02-SumArray/SumArray.js
@@ -1,6 +1,15 @@
 function SumArray (arr, n) {
   // Your code here:
+  for(let i = 0; i < arr.length-1;i++){
+    if((arr[i] + arr[i+1]) === n)
+      return true;
+  }
 
+  return false;
 }
+
+console.log(SumArray([2,4,5,9],9));
+console.log(SumArray([2,4,5,9],12));
+
 
 module.exports = SumArray
